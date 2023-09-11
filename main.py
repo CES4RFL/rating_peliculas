d23844054284d6146a3593e56d617e285c86509d
César Augusto Flores López <cesar.afl@outlook.com>
2023-09-11 01:23:48 -0600

    primer commit

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-import requests
-from string import Template
-import json
-
-apiKey="ab1ad77639ff09b837da76a4cc663d17"
-urlSearch= Template('https://api.themoviedb.org/3/search/movie?query=$n1&api_key=$n2')
-
+movies = {"Avengers: Infinity War":"PG-13 - Con supervision de padres"
+,"Tortugas Ninja: Caos Mutante":"PG - Para todas las edades"
+,"Frozen":"PG - Para todas las edades"
+,"It":"R - Restringida"}
 while(True):
-    print("Si deseas salir solo escribe salir")
-    palabra=input("Ingresa una palabra de la pelicula que quieres buscar\n\n")
+    print("Escribe la palabra salir para terminar con la ejecucion del programa\n")
 
-    if(palabra=="salir"):
+    for key in movies:
+        print("* "+key)
+
+    inputMovie=input("\nIngresa el nombre de alguna de las peliculas para saber su clasificacion:\n")
+
+    if(inputMovie=="salir"):
         break
 
-    resultado =  requests.get(urlSearch.substitute(n1=palabra, n2=apiKey))
-    payload=json.loads(resultado.text)
+    movie = movies.get(inputMovie)
 
-    if(len(payload['results'])>0):
-        result = payload['results'][0]
-        print("Titulo: "+ str(result['original_title']))
-        print("Puntuacion: "+ str(result['vote_average']))
-        print("\n")
+    if(movie == None):
+        print("La pelicula no existe por favor ingresa otra pelicula\n ")
     else:
-        print("No se encontro informacion")
+        print("La clasificacion de la pelicula: " + inputMovie + " es: "+movie+"\n")
 
